c5c9ace2e8a1177fac3b1cb5036970c0586777de
noyna <pimpaaa2003@gmail.com>
2024-07-04 16:21:08 +0700

    delete temp

--- nodejs/www/soundwave/dashboard/app.js
+++ nodejs/www/soundwave/dashboard/app.js
@@ -91,11 +91,7 @@ window.setInterval(function () {
         const sound8 = r.data.filter((a) => { return a.stationname == '09' }).map(a => ({ x: new Date(a.ts).getTime(), y: Number(a.sound_level) }))
         const sound9 = r.data.filter((a) => { return a.stationname == '10' }).map(a => ({ x: new Date(a.ts).getTime(), y: Number(a.sound_level) }))
 
-        //console.log(temp)
-        // console.log(charttemp)
 
-        // charttemp.updateSeries([{ data: temp }, { data: temp1 }, { data: temp2 }, { data: temp3 }, { data: temp4 }, { data: temp5 }, { data: temp6 }, { data: temp7 }, { data: temp8 }, { data: temp9 }])
-        // chartRh.updateSeries([{ data: rh }, { data: rh1 }, { data: rh2 }, { data: rh3 }, { data: rh4 }, { data: rh5 }, { data: rh6 }, { data: rh7 }, { data: rh8 }, { data: rh9 }])
         chartsound.updateSeries([{ data: sound }, { data: sound1 }, { data: sound2 }, { data: sound3 }, { data: sound4 }, { data: sound5 }, { data: sound6 }, { data: sound7 }, { data: sound8 }, { data: sound9 }])
     })
 }, 3000)
