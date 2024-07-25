3233983ae6ce836f078fc4a5df65d91100d63cda
noyna <pimpaaa2003@gmail.com>
2024-07-25 16:15:32 +0700

    set database

--- gdal/app.py
+++ gdal/app.py
@@ -29,39 +29,39 @@ try:
         isopath = "./json"
         tiffpath = "./tiff"
         shppath = "./shp"
-        sql = f'''SELECT stationname, lat, lng, AVG(sound_level) AS savg \
+        sql = f'''SELECT geom, stationname, lat, lng, AVG(sound_level) AS savg \
                     FROM public.v_parks_sound \
                     WHERE datetime >= '2024-07-18 01:00:00' \
                     AND datetime < '2024-07-18 02:00:00' \
-                    GROUP BY stationname, lat, lng'''
-
+                    GROUP BY geom, stationname, lat, lng'''
+        print(sql)
         stat_shp = f'''stat_shp.shp'''
         cmd = f'''ogr2ogr -overwrite -f "ESRI Shapefile" {stat_shp} PG:"host={dbServer} user={dbUser} dbname={dbName} password={dbPass}" -sql "{sql}"'''
         os.system(cmd)
         print("shp created")
 
-        # output_raster = f'idw.tif'
+        output_raster = f'idw.tif'
+        cmd = f'''gdal_grid -a invdist:power=2:max_points=15:min_points=3 \
+        -outsize 100 100 \
+        -of GTiff \
+        -zfield "{fld}" \
+        {stat_shp} {output_raster}'''
+        os.system(cmd)
+        print("idw created")
 
-        # cmd = f'''gdal_grid -a invdist:power=2:max_points=15:min_points=3 \
-        # -outsize 100 100 \
-        # -of GTiff \
-        # -zfield "{fld}" \
-        # {stat_shp} {output_raster}'''
-        # os.system(cmd)
-        # print("idw created")
+        output_geojson_filename = f'isoline.geojson'
+        cmd = ['gdal_contour', '-a', fld, '-i', str(interval), output_raster, output_geojson_filename]
 
-        # output_geojson_filename = f'isoline.geojson'
-        # cmd = ['gdal_contour', '-a', fld, '-i', str(interval), output_raster, output_geojson_filename]
-
-        try:
-            subprocess.check_call(cmd)
-            with open(output_geojson_filename, 'r') as geojson_file:
-                geojson_data = json.load(geojson_file)
-            return jsonify(geojson_data), 200
-        except subprocess.CalledProcessError:
-            return jsonify(error='Error running gdal_contour'), 500
-        except FileNotFoundError:
-            return jsonify(error='GeoJSON file not found'), 404
+        return jsonify(status='success!!'), 200
+        # try:
+        #     subprocess.check_call(cmd)
+        #     with open(output_geojson_filename, 'r') as geojson_file:
+        #         geojson_data = json.load(geojson_file)
+        #     return jsonify(geojson_data), 200
+        # except subprocess.CalledProcessError:
+        #     return jsonify(error='Error running gdal_contour'), 500
+        # except FileNotFoundError:
+        #     return jsonify(error='GeoJSON file not found'), 404
 
 except ImportError:
     print("GDAL's Python bindings are not installed!")
