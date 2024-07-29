70a929d0a999c84a3b92eb60746af92f1d287516
noyna <pimpaaa2003@gmail.com>
2024-07-29 14:30:45 +0700

    test dt7

--- gdal/app.py
+++ gdal/app.py
@@ -31,8 +31,8 @@ try:
         shppath = "./shp"
         sql = f'''SELECT geom, stationname, lat, lng, AVG(sound_level) AS savg \
                     FROM public.v_parks_sound \
-                    WHERE datetime >= '2024-07-18 01:00:00' \
-                    AND datetime < '2024-07-18 02:00:00' \
+                    WHERE dt7 >= '2024-07-23 08:00:00' \
+                    AND dt7 < '2024-07-23 09:00:00' \
                     GROUP BY geom, stationname, lat, lng'''
         print(sql)
         stat_shp = f'''stat_shp.shp'''
