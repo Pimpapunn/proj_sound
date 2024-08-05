275188a38c66a3805e7a020a0aa55866f2e6fec1
noyna <pimpaaa2003@gmail.com>
2024-08-05 09:17:02 +0700

    .

--- nodejs/www/soundwave/home/app.js
+++ nodejs/www/soundwave/home/app.js
@@ -1,3 +1,10 @@
 function toggleHam(x) {
     x.classList.toggle("change");
+
+    let myMenu = document.getElementById('myMenu');
+    is(myMenu.className === 'menu') {
+        myMenu.className + - 'menu-active'
+    } else {
+        myMenu.className = 'menu';
+    }
 }
