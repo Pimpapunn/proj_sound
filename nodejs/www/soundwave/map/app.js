47d69f7fc8f4e927b59c1dc71f010ddc1627cae0
noyna <pimpaaa2003@gmail.com>
2024-08-05 09:01:04 +0700

    .

--- nodejs/www/soundwave/map/app.js
+++ nodejs/www/soundwave/map/app.js
@@ -1,5 +1,4 @@
 
-
 const map = L.map('map').setView([18.800938160747965, 98.95212346404927], 18);
 
 
@@ -11,20 +10,6 @@ var osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 var Esri_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
     attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
 });
-
-// const url = "https://jumbomap.cmu.ac.th/controls/getApTableGuest.php"
-// axios.get(url).then((i) => {
-
-
-//     i.data.forEach(e => {
-//         console.log(e)
-//         L.circleMarker([e.lat, e.lng], {
-//             radius: 5, color: "red"
-//         }).addTo(map).bindPopup(`apName=${e.apName}
-//             <br>status=${e.status}
-//             <br>totalUser=${e.totalUser}`);
-//     });
-// })
 
 const basemap = {
     osm: osm.addTo(map),
