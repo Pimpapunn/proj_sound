451bb88d370c689a20e053289397088448c3d71f
noyna <pimpaaa2003@gmail.com>
2024-08-08 15:08:47 +0700

    .

--- nodejs/www/soundwave/map/app.js
+++ nodejs/www/soundwave/map/app.js
@@ -11,6 +11,10 @@ var Esri_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest
     attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
 });
 
+var Esri_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+    attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+});
+
 const basemap = {
     osm: osm.addTo(map),
     Esri_WorldImagery: Esri_WorldImagery
