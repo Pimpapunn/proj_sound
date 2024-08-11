3cb8e958b06d7342821778406db35f577917c678
noyna <pimpaaa2003@gmail.com>
2024-08-11 13:31:11 +0700

    .

--- nodejs/www/soundwave/map/app.js
+++ nodejs/www/soundwave/map/app.js
@@ -1,6 +1,5 @@
 const map = L.map('map').setView([18.800938160747965, 98.95212346404927], 18);
 
-
 var osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
