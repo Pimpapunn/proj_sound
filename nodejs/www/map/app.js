b1970ff7a4488d026c7aa7aa233feaa9f6d5966a
noyna <pimpaaa2003@gmail.com>
2024-08-19 19:16:04 +0700

    min max

--- nodejs/www/map/app.js
+++ nodejs/www/map/app.js
@@ -65,8 +65,6 @@ async function showGeotiff(dayName, timeStart, timeEnd) {
     setTimeout(() => {
         const min = windSpeedLayer.options.renderer.parent.min;
         const max = windSpeedLayer.options.renderer.parent.max;
-
-        // Print the min and max values to the console
         console.log("Min value:", min);
         console.log("Max value:", max);
 
@@ -80,9 +78,9 @@ async function showGeotiff(dayName, timeStart, timeEnd) {
             max
         );
 
-        document.getElementById("displayMin").value = min;
-        document.getElementById("displayMax").value = max;
-    }, 1000);
+        document.getElementById("displayMin").value = min
+        document.getElementById("displayMax").value = max
+    }, 1000)
 
 
     // VECTOR ARROW EG
